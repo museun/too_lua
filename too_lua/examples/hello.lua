51e980ab2beec7a6ab06b9199f6674d5afa54b03
museun <museun@users.noreply.github.com>
2025-02-17 10:42:01 -0500

    reduce skeleton example

--- too_lua/examples/hello.lua
+++ too_lua/examples/hello.lua
@@ -1,27 +1,6 @@
--- local function children(ui)
---     for i = 0, 100 do
---         ui.label(string.format("hello: %d", i))
---     end
--- end
-
-local checked = Value.new(false)
-
 ---@param ui ui
 return function(ui)
-    -- ui.vertical {
-    --     scrollable = true,
-    --     cross_align = CrossAlign.fill,
-    --     children(ui)
-    -- }
-
     ui.center {
-        -- I don't like this name, at all
-        ui.constrained {
-            constraint = Constraint.min_width(20),
-            ui.toggle_switch {
-                class = Toggle.large,
-                value = checked,
-            }
-        }
+        ui.label "hello world"
     }
 end
--- too_lua/examples/hello.rs
+++ too_lua/examples/hello.rs
@@ -2,16 +2,12 @@ use too::RunConfig;
 use too_lua::{Bindings, Proxies};
 
 fn main() -> std::io::Result<()> {
-    // let server_addr = format!("127.0.0.1:{}", puffin_http::DEFAULT_PORT);
-    // let _puffin_server = puffin_http::Server::new(&server_addr).unwrap();
-    // profiling::puffin::set_scopes_on(true);
-
     too_lua::Application::new("./too_lua/examples/hello.lua")
         .reload_keybind('r')
         .with_bindings(Bindings::default_bindings())
         .with_proxies(Proxies::default_proxies())
         .config(RunConfig {
-            debug: too::view::DebugMode::Rolling,
+            debug: too::view::DebugMode::PerFrame,
             debug_anchor: too::layout::Anchor2::LEFT_TOP,
             ..RunConfig::default()
         })
