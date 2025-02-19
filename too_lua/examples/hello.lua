d3a4233364b3fb87087b0463fa4c5e6f1af8bbee
museun <museun@users.noreply.github.com>
2025-02-18 19:07:42 -0500

    remove unused unicode-width dep

--- too_lua/examples/hello.lua
+++ too_lua/examples/hello.lua
@@ -1,6 +1,26 @@
+local something = Value.persist("test", 0.1)
+
+rt.spawn(function()
+    repeat
+        something.value = something.value + 0.001
+        rt.sleep_ms(10)
+    until false
+end)
+
 ---@param ui ui
 return function(ui)
     ui.center {
-        ui.label "hello world"
+        ui.vertical {
+            gap = 1,
+            cross_align = CrossAlign.center,
+            justify = Justify.space_between,
+            ui.label(lazy { function() return string.format("%0.2f", something.value) end }),
+            ui.slider {
+                value = something,
+            },
+            ui.progress {
+                value = something,
+            }
+        }
     }
 end
--- too_lua/examples/hello.rs
+++ too_lua/examples/hello.rs
@@ -1,16 +1,28 @@
-use too::RunConfig;
+use std::future::Future;
+
 use too_lua::{Bindings, Proxies};
 
+fn start<R>(f: impl Future<Output = R> + Send + Sync + 'static) -> R {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(f)
+}
+
 fn main() -> std::io::Result<()> {
-    too_lua::Application::new("./too_lua/examples/hello.lua")
-        .reload_keybind('r')
-        .with_bindings(Bindings::default_bindings())
-        .with_proxies(Proxies::default_proxies())
-        .config(RunConfig {
-            debug: too::view::DebugMode::PerFrame,
-            debug_anchor: too::layout::Anchor2::LEFT_TOP,
-            ..RunConfig::default()
-        })
-        .watch_timeout(std::time::Duration::from_secs(1))
-        .run()
+    start(async move {
+        too_lua::Application::new("./too_lua/examples/hello.lua")
+            .reload_keybind('r')
+            .with_bindings(Bindings::default_bindings())
+            .with_proxies(Proxies::default_proxies())
+            .config(too::RunConfig {
+                debug: too::view::DebugMode::Rolling,
+                debug_anchor: too::layout::Anchor2::LEFT_TOP,
+                ..too::RunConfig::default()
+            })
+            .watch_timeout(std::time::Duration::from_secs(1))
+            .run()
+            .await
+    })
 }
