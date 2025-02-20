fdd43702ea9dc346b5df64706f4375cf7399f7dc
museun <museun@users.noreply.github.com>
2025-02-20 14:42:06 -0500

    reset hello.lua

--- too_lua/examples/hello.lua
+++ too_lua/examples/hello.lua
@@ -1,15 +1,6 @@
 ---@param ui ui
-local function show_nodes(ui)
-    for i = 0, 10000 do
-        ui.label(string.format("hello, world. #%d", i))
-    end
-end
-
----@param ui ui
 return function(ui)
-    ui.vertical {
-        scrollable = true,
-        cross_align = CrossAlign.fill,
-        show_nodes(ui)
+    ui.center {
+        ui.label "hello world"
     }
 end
